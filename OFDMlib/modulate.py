6ca783759941fd4c9dfcb2bc4306f02f5a7f35d7
Nicholas Corey <nicholas.corey@gmail.com>
2014-08-18 23:34:10 -0700

    Basic OFDM encode is on its way

--- OFDMlib/modulate.py
+++ OFDMlib/modulate.py
@@ -6,18 +6,66 @@ SAMPLE_RATE = 48000
 
 
 def stream(inStream, outStream, bandwidth, symbolDuration):
-	spacing = 1.0 / symbolDuration
-	numChannels = int(bandwidth / spacing)
-	freqs = [(c*float(bandwidth)+bandwidth/2.0)/numChannels for c in range(numChannels)]
-	channels = [qam.Qam64(f, symbolDuration, SAMPLE_RATE) for f in freqs]
+	qamType = qam.Qam64
+	ofdm = OFDM(bandwidth, symbolDuration, qamType)
+	bitReader = BitReader(inStream, ofdm.getSymbolSize())
 
-	data = readStream(inStream)
-	while data <> "":
-		outStream.write(data)
-		data = readStream(inStream)
+	while True:
+		data = bitReader.read()
+		if data == None:
+			break
+		print data
+		print ofdm.encode(data)
 
 
 
-def readStream(inStream):
-	return inStream.read(30)
+
+class BitReader:
+	def __init__(self, inStream, numBits):
+		self.inStream = inStream
+		self.numBits = numBits
+		self.buffer = ''
+
+	def read(self):
+		try:
+			while len(self.buffer) < self.numBits:
+				self.buffer += self.toBinary(self.readByte())
+			output = self.buffer[:self.numBits]
+			self.buffer = self.buffer[self.numBits:]
+			return output
+		except EOFError:
+			if self.buffer <> '':
+				output = (self.buffer + ('0'*self.numBits))[:self.numBits]
+				self.buffer = ''
+				return output
+			else:
+				return None
+			
+	def toBinary(self, char):
+		return ('00000000' + bin(ord(char))[2:])[-8:]
+	
+	def readByte(self):
+		output = self.inStream.read(1)
+		if output == "":
+			raise EOFError
+		return output
+
+
+
+class OFDM:
+	def __init__(self, bandwidth, symbolDuration, qamType):
+		spacing = 1.0 / symbolDuration
+		numChannels = int(bandwidth / spacing)
+		freqs = [(c*float(bandwidth)+bandwidth/2.0)/numChannels for c in range(numChannels)]
+		self.qamType = qamType
+		self.channels = [qamType(f, symbolDuration, SAMPLE_RATE) for f in freqs]
+		
+	def getSymbolSize(self):
+		return self.qamType.getSymbolSize() * len(self.channels)
+
+	def encodeSymbol(self, bits):
+		assert len(bits) == self.getSymbolSize()
+		return sum([self.channels[i].waveOut(bits[i*self.qamType.getSymbolSize():(i+1)*self.qamType.getSymbolSize()]) for i in range(len(self.channels))])
+
+
 
--- OFDMlib/qam.py
+++ OFDMlib/qam.py
@@ -5,22 +5,23 @@ from scipy import pi
 
 
 class Qam64:
-	def __init__(freq, duration, sampleRate):
+	def __init__(self, freq, duration, sampleRate):
 		self.freq = freq
 		self.duration = duration
 		self.sampleRate = sampleRate
 		self.grayCode = [0b000, 0b001, 0b011, 0b010, 0b110, 0b111, 0b101, 0b100]
 	
-	def getInputBitSize():
+	@staticmethod
+	def getSymbolSize():
 		return 6
 		
-	def waveOut(bits):
-		a = amplitude(bits)
+	def waveOut(self, bits):
+		a = _amplitude(bits)
 		return 	a[0] * scipy.sin(2 * pi * self.freq * scipy.linspace(0, self.duration, self.duration*self.sampleRate)) + \
 				a[1] * scipy.cos(2 * pi * self.freq * scipy.linspace(0, self.duration, self.duration*self.sampleRate))
 
-	def amplitude(bits):
-		high = self.grayCode.index((bits % 64) >> 3) / 7.0 * 2 - 1
-		low = self.grayCode.index(bits % 8) / 7.0 * 2 - 1
+	def _amplitude(self, bits):
+		high = self.grayCode.index(int(bits[0:3], 2)) / 7.0 * 2 - 1
+		low = self.grayCode.index(int(bits[3:6], 2)) / 7.0 * 2 - 1
 		return high, low
 
